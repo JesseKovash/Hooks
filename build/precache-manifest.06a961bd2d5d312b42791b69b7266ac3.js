9698762ce171a7a2f4996475deea730984cfe7b5
Jesse Kovash <jessekovash@gmail.com>
2022-04-25 18:03:52 -0600

    initial commit

---
+++ build/precache-manifest.06a961bd2d5d312b42791b69b7266ac3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ba96f3fd70b935ad912c928cf6a4c09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72c95d9a3e45fa17e598",
+    "url": "/static/css/main.bc874b77.chunk.css"
+  },
+  {
+    "revision": "9942f3611f72e1086ce3",
+    "url": "/static/js/2.1de42018.chunk.js"
+  },
+  {
+    "revision": "72c95d9a3e45fa17e598",
+    "url": "/static/js/main.1de86dff.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
